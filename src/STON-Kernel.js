9401782171d41ea658fac161821a100b913151c1
Jupiter Jones <jupiter.jones@mail.com>
2015-06-12 10:01:06 +1000

    124 runs, 30 passes, 85 errors, 9 failures - ugly hack for Characters. Needs a more considered solution.

--- src/STON-Kernel.js
+++ src/STON-Kernel.js
@@ -7,6 +7,37 @@ $core.packages["STON-Kernel"].transport = {"type":"amd","amdNamespace":"amber-st
 $core.addClass('Bag', $globals.Set, [], 'STON-Kernel');
 
 
+$core.addClass('Character', $globals.String, [], 'STON-Kernel');
+//>>excludeStart("ide", pragmas.excludeIdeData);
+$globals.Character.comment="This is a hack to support transport of Characters in STON.\x0aIt represents stings with only one element ie. self size == 1.";
+//>>excludeEnd("ide");
+
+$core.addMethod(
+$core.method({
+selector: "fromSton:",
+protocol: 'not yet classified',
+fn: function (stonReader) {
+var self=this;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+return $core.withContext(function($ctx1) {
+//>>excludeEnd("ctx");
+var $1;
+$1=$recv($recv(stonReader)._parseListSingleton())._first();
+return $1;
+//>>excludeStart("ctx", pragmas.excludeDebugContexts);
+}, function($ctx1) {$ctx1.fill(self,"fromSton:",{stonReader:stonReader},$globals.Character.klass)});
+//>>excludeEnd("ctx");
+},
+//>>excludeStart("ide", pragmas.excludeIdeData);
+args: ["stonReader"],
+source: "fromSton: stonReader\x0a\x09^ stonReader parseListSingleton first",
+referencedClasses: [],
+//>>excludeEnd("ide");
+messageSends: ["first", "parseListSingleton"]
+}),
+$globals.Character.klass);
+
+
 $core.addClass('IdentityDictionary', $globals.Dictionary, [], 'STON-Kernel');
 
 });
